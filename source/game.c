1b270e83a53a37eeee8d5988a5fb6af7d90e1e8b
MathisMartin31 <mathis.martin31@gmail.com>
2026-06-16 19:09:46 +0200

    Make unbeaten blinds list initialization automatic

--- source/game.c
+++ source/game.c
@@ -199,10 +199,6 @@ void game_init()
     g_game_vars.best_hand_score = 0;
     for (int i = 0; i < HAND_TYPE_MAX; i++)
         g_game_vars.nb_played_hands[i] = 0;
-
-    // Initialize/reset unbeaten Boss/Showdown Blinds so they are all available
-    init_unbeaten_blinds_list(false);
-    init_unbeaten_blinds_list(true);
 }
 
 void game_reset()
